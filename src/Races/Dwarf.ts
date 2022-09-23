42c10ebe2c59154e62b1b106a36421411b80456d
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2022-09-22 21:36:03 -0300

    Cria classe Dwarf

---
+++ src/Races/Dwarf.ts
@@ -0,0 +1,22 @@
+import Race from './Race';
+
+class Dwarf extends Race {
+  readonly _maxLifePoints: number;
+  private static _count = 0; 
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    this._maxLifePoints = 80;
+    Dwarf._count = +1;
+  }
+
+  public static createdRacesInstances(): number {
+    return Dwarf._count;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+}
+
+export default Dwarf;
