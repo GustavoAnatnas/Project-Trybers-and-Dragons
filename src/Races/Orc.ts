207e1703cd8979b7225a2c3f92a03ee5b81ad9bb
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2022-09-22 21:47:09 -0300

    Cria classe Orc

---
+++ src/Races/Orc.ts
@@ -0,0 +1,22 @@
+import Race from './Race';
+
+class Orc extends Race {
+  readonly _maxLifePoints: number;
+  private static _count = 0; 
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    this._maxLifePoints = 74;
+    Orc._count += 1;
+  }
+
+  public static createdRacesInstances(): number {
+    return Orc._count;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+}
+
+export default Orc;
