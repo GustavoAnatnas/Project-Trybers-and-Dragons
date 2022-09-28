4f42a7b98f1c9a83203ddaa7564e9b5e2699c60b
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2022-09-27 22:16:47 -0300

    Cria classe Monster

---
+++ src/Monster.ts
@@ -0,0 +1,38 @@
+import { SimpleFighter } from './Fighter';
+
+class Monster implements SimpleFighter {
+  private _lifePoints: number;
+  private _strength: number;
+
+  constructor() {
+    this._lifePoints = 85;
+    this._strength = 63;
+  }
+
+  get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  get strength(): number {
+    return this._strength;
+  }
+
+  receiveDamage(attackPoints: number): number {
+    const receivedDamage = this._lifePoints - attackPoints;
+
+    if (receivedDamage > 0) {
+      this._lifePoints -= receivedDamage;
+      if (this._lifePoints <= 0) {
+        this._lifePoints = -1;
+      }
+    }
+    return this._lifePoints;
+  }
+
+  attack(enemy: SimpleFighter): number {
+    const attackPoints = this._strength;
+    return enemy.receiveDamage(attackPoints);
+  }
+}
+
+export default Monster;
